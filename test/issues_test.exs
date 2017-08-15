85ccb0d4b972696e08ca33cb1120fed345540efb
Adam Florczak <adam.florczak@me.com>
2017-08-15 11:14:42 +0200

    Github fetch helpers

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -1,8 +1,18 @@
-defmodule IssuesTest do
+defmodule CLITest do
   use ExUnit.Case
   doctest Issues
+  import Issues.CLI, only: [ parse_args: 1 ]
 
-  test "greets the world" do
-    assert Issues.hello() == :world
+  test "help returned by passing -h or --help options" do
+    assert parse_args([ "-h", "anything" ]) == :help
+    assert parse_args([ "--help", "anything" ]) == :help
+  end
+
+  test "three values returned if three given" do
+    assert parse_args([ "user", "project", "5" ]) == { "user", "project", 5 }
+  end
+
+  test "count is defaulted whe two values are given" do
+    assert parse_args([ "user", "project" ]) == { "user", "project", 4 }
   end
 end
